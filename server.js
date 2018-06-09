46c8eed1626ba6dd252bb57cc25d6c1cea88b297
Shaela Sterling <msshae23_09@yahoo.com>
2018-06-09 07:55:24 -0400

    updated database authentication

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Requiring our Note and Article models
 var Note = require("./models/post.js");
 var Article = require("./models/articles.js");
 
-// Our scraping tools
+// Scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
-// Initialize Express
 var app = express();
 
-// Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -32,23 +28,21 @@ app.set("view engine", "handlebars");
 // Make public a static dir
 app.use(express.static("public"));
 
-// Database configuration wiht mongoose
-mongoose.connect("mongodb://heroku_0x0wl2kv:l4dt8qkicrnc9mdfjobo8vd0k3@ds127034.mlab.com:27034/heroku_0x0wl2kv");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect("mongodb://heroku_project123:12345ste@ds253840.mlab.com:53840/heroku_st7zjn7b");
 var db = mongoose.connection;
 
-// Show any mongoose errors
 db.on("error", function(error) {
 	console.log("Mongoose Error: ", error);
 });
 
-// Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
 	console.log("Mongoose connection successful.");
 });
 
 // Routes
-// A GET request to scrape the echojs website
-// 
+// A GET request to scrape the nypost website
 app.get("/", function(req, res) {
   // Grab every doc in the Articles array
   Article.find({}, function(error, doc) {
@@ -56,7 +50,6 @@ app.get("/", function(req, res) {
     if (error) {
       console.log(error);
     }
-    // Or send the doc to the browser as a json object
     else {
       //res.json(doc);
       res.render("index", {articles: doc});
@@ -64,40 +57,29 @@ app.get("/", function(req, res) {
   });
 });
 
-// A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
-	// First, we grab the body of the html with request
 	request("http://nypost.com/living/", function(error, response, html) {
-		// Then, we load that into cheerio and save it to $ for a shorthand selector
 		var $ = cheerio.load(html);
-		// Now, we grab every h2 within an article tag, and do the following:
 		$("article h3").each(function(i, element) {
-			// Save an empty result object
 			var result = {};
 
-			// Add the text and href of every link, and save them as properties of the result object
 			result.title = $(this).children("a").text();
 			result.link = $(this).children("a").attr("href");
 
-			// Using our Article model, create a new entry
-			// This effectively passes the result object to the entry (and the title and link)
 			var entry = new Article(result);
 
-			// Now, save that entry to the db
 			entry.save(function(err, doc) {
 				// Log any errors
 				if (err) {
 					console.log(err);
 				}
-				// Or log the doc
 				else {
 					console.log(doc);
 				}
 			});
 		});
 	});
-	// Tell the browser that we finished scraping the text
-	// res.send("Scrape Complete");
+	
 	res.redirect("/");
 });
 
@@ -117,20 +99,15 @@ app.get("/articles", function(req, res) {
 	});
 });
 
-// Grab an article by it's ObjectId
 app.get("/articles/:id", function(req, res) {
-	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db.
 	Article.findOne({ "_id": req.params.id })
-	// .. and populate all of the notes associated with it
 	.populate("note")
-	// now, execute our query
 	.exec(function(error, doc) {
 		// Log any errors
 		if (error) {
 			console.log(error);
 		}
-		// Otherwise, send the doc to the browser as a json object
-		else {
+				else {
 			res.json(doc);
 		}
 	});
@@ -138,10 +115,8 @@ app.get("/articles/:id", function(req, res) {
 
 // Create a new note or replace an existing note
 app.post("/articles/:id", function(req, res) {
-	// Create a new note and pass the req.body to the entry
 	var newNote = new Note(req.body);
 
-	// And save the new note the db
 	newNote.save(function(error, doc) {
 		// Log any errors
 		if (error) {
@@ -149,16 +124,12 @@ app.post("/articles/:id", function(req, res) {
 		}
 		// Otherwise
 		else {
-			// Use the article id to find and update it's note
 			Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-			// Execute the above query
 			.exec(function(err, doc) {
-				// Log any errors
 				if (err) {
 					console.log(err);
 				}
 				else {
-					// Or send the document to the browser
 					res.send(doc);
 				}
 			});
@@ -166,7 +137,7 @@ app.post("/articles/:id", function(req, res) {
 	});
 });
 
-// Listen on port 3000
-app.listen(process.env.PORT || 3000, function() {
-	console.log("App running on port 3000.");
+// Listen on port 3010
+app.listen(process.env.PORT || 3010, function() {
+	console.log("App running on port 3010.");
 });
